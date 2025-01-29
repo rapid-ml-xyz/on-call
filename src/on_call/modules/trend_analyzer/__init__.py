68896e01e74183b46f13ba2db4dca5a22b587cfe
Shrey <shrey.cha@gmail.com>
2025-01-29 18:33:19 +0530

    ft(modules): Trend Analyzer

---
+++ src/on_call/modules/trend_analyzer/__init__.py
@@ -0,0 +1,25 @@
+from .trend_analyzer import TrendAnalyzer
+from .trend_patterns import (
+    TrendType,
+    SeasonalityType,
+    TrendResult,
+    SeasonalityResult,
+    OutlierResult,
+    RandomnessResult,
+    AutocorrelationResult,
+    HeteroscedasticityResult,
+    TrendAnalysisResult,
+)
+
+__all__ = [
+    "TrendAnalyzer",
+    "TrendType",
+    "SeasonalityType",
+    "TrendResult",
+    "SeasonalityResult",
+    "OutlierResult",
+    "RandomnessResult",
+    "AutocorrelationResult",
+    "HeteroscedasticityResult",
+    "TrendAnalysisResult",
+]
