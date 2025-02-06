bdcffe8495269660ced57c159a562dea88f3f6de
Anant Chandra <anantcha@usc.edu>
2025-02-06 19:16:12 +0530

    Add window flagger module & remove bloat

--- src/on_call/modules/__init__.py
+++ src/on_call/modules/__init__.py
@@ -1,9 +1,9 @@
 from .baseline_analyzer import BaselineAnalyzer
+from .flag_critical_windows import FlagCriticalWindows
 from .impact_window_analyzer import ImpactWindowAnalyzer
 from .imports import ImportsModule
 from .placeholder import do_nothing
 from .time_series_reports import TimeSeriesReports
 from .window_visualizations import WindowVisualizations
 
-__all__ = ['do_nothing', 'BaselineAnalyzer', 'ImpactWindowAnalyzer', 'ImportsModule', 'TimeSeriesReports',
-           'WindowVisualizations']
+__all__ = ['FlagCriticalWindows', 'ImportsModule', 'TimeSeriesReports', 'WindowVisualizations']
