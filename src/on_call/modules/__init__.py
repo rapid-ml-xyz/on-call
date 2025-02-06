11ecd642603df036c38385744500637d0a9828c4
Anant Chandra <anantcha@usc.edu>
2025-02-06 23:16:51 +0530

    Add feature visualizations module

--- src/on_call/modules/__init__.py
+++ src/on_call/modules/__init__.py
@@ -1,9 +1,10 @@
 from .feature_analyses import FeatureAnalyses
+from .feature_visualizations import FeatureVisualizations
 from .flag_critical_windows import FlagCriticalWindows
 from .imports import ImportsModule
 from .time_series_reports import TimeSeriesReports
 from .time_series_summary import TimeSeriesSummary
 from .window_visualizations import WindowVisualizations
 
-__all__ = ['FeatureAnalyses', 'FlagCriticalWindows', 'ImportsModule', 'TimeSeriesReports', 'TimeSeriesSummary',
-           'WindowVisualizations']
+__all__ = ['FeatureAnalyses', 'FeatureVisualizations', 'FlagCriticalWindows', 'ImportsModule', 'TimeSeriesReports',
+           'TimeSeriesSummary', 'WindowVisualizations']
