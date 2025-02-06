d15d3773d5bdb725c2621e0d7eb5bc9c2ce8dd1e
Anant Chandra <anantcha@usc.edu>
2025-02-06 23:33:26 +0530

    Add feature summary module

---
+++ src/on_call/modules/feature_summary/__init__.py
@@ -0,0 +1,3 @@
+from .workflow import FeatureSummary
+
+__all__ = ['FeatureSummary']
--- src/on_call/workflow/setup.py
+++ src/on_call/workflow/setup.py
@@ -1,5 +1,5 @@
-from on_call.modules import FeatureAnalyses, FeatureVisualizations, FlagCriticalWindows, ImportsModule, \
-    TimeSeriesReports, TimeSeriesSummary, WindowVisualizations
+from on_call.modules import FeatureAnalyses, FeatureSummary, FeatureVisualizations, FlagCriticalWindows, \
+    ImportsModule, TimeSeriesReports, TimeSeriesSummary, WindowVisualizations
 from on_call.orchestrator.engines import LangGraphOrchestrator, LangGraphToolWrapper, LangGraphMessageState
 from on_call.orchestrator import EdgeConfig, NodeConfig, NodeType, RouteType
 from on_call.workflow.enums import Step
@@ -17,7 +17,8 @@ def setup_analysis_workflow() -> LangGraphOrchestrator:
             (Step.FLAG_CRITICAL_WINDOWS, FlagCriticalWindows),
             (Step.TIME_SERIES_SUMMARY, TimeSeriesSummary),
             (Step.FEATURE_ANALYSES, FeatureAnalyses),
-            (Step.FEATURE_VISUALIZATIONS, FeatureVisualizations)
+            (Step.FEATURE_VISUALIZATIONS, FeatureVisualizations),
+            (Step.FEATURE_SUMMARY, FeatureSummary)
         ]
     ]
 
