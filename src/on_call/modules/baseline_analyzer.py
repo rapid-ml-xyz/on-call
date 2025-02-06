ea3b580ab281c61233b1a6f2ac3fe42f07ba8216
Anant Chandra <anantcha@usc.edu>
2025-02-06 19:39:16 +0530

    Delete unused/ placeholder modules

--- src/on_call/modules/baseline_analyzer.py
+++
@@ -1,9 +0,0 @@
-from .base_module import BaseModule
-from ..orchestrator import WorkflowState
-
-
-class BaselineAnalyzer(BaseModule):
-    def run(self) -> WorkflowState:
-        model = self.state['model']
-        self.state['baseline_results'] = {"ref": model.prediction_col}
-        return WorkflowState(self.state)
